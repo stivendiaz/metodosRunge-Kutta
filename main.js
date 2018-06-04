e3574ebc50ed98abd933bf937bcf43cfe8d4a424
stivendiaz <stivendiazagudelo@gmail.com>
2018-06-04 14:50:31 -0500

    Añadimos cosas faltantes

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(".euler").click(function (e) {
 
     console.log(parser.eval('f(x, y) = ' + m + '')); // f(x, y)
 
-    euler(parseFloat($("#X").val()), parseFloat($("#Y").val()), parseFloat($("#H").val()));
+    euler(parseFloat($("#Xmin").val()),parseFloat($("#Xmax").val()), parseFloat($("#Y").val()), parseFloat($("#H").val()));
 
 });
 $(".mejorado").click(function (e) {
@@ -18,7 +18,7 @@ $(".mejorado").click(function (e) {
 
     console.log(parser.eval('f(x, y) = ' + m + '')); // f(x, y)
 
-    euler_mejorado(parseFloat($("#X").val()), parseFloat($("#Y").val()), parseFloat($("#H").val()));
+    euler_mejorado(parseFloat($("#Xmin").val()),parseFloat($("#Xmax").val()), parseFloat($("#Y").val()), parseFloat($("#H").val()));
 
 });
 $(".ralston").click(function (e) {
@@ -27,33 +27,42 @@ $(".ralston").click(function (e) {
 
     console.log(parser.eval('f(x, y) = ' + m + '')); // f(x, y)
 
-    ralston(parseFloat($("#X").val()), parseFloat($("#Y").val()), parseFloat($("#H").val()));
+    ralston(parseFloat($("#Xmin").val()),parseFloat($("#Xmax").val()), parseFloat($("#Y").val()), parseFloat($("#H").val()));
 
 });
 
-function euler(x, y, h) {
+function euler(x,xmax, y, h) {
     var yn, fun;
 
 
     var i = 0;
 
-    while (x <= 2) {
+    while (x < xmax) {
+
+        //
+        // parseFloat(
+        //     (parseFloat('2.3') + parseFloat('2.4')).toFixed(2))
+        let sum = parseFloat('2.3') + parseFloat('2.4');
+        // parseFloat(sum.toFixed(2));
 
         i += 1;
 
         yn = y + h * (parser.eval('f(' + x + ',' + y + ')'));
         console.log(" # " + i + " Yn+1 = " + yn + " Yn = " + y + " x = " + x + "\n");
-        $(".tbody").prepend(" <tr> <th>" + yn + "</th> <th>" + y + "</th> <th>" + x + "</th>  </tr>")
+        $(".tbody").prepend(`<tr><th>${yn}</th><th>${y}</th><th>${x.toFixed(2)}</th></tr>`);
+        // $(".tbody").prepend(" <tr> <th>" + yn + "</th> <th>" + y + "</th> <th>" + x + "</th>  </tr>")
 
-        x += parseFloat(h);
-        console.log(x)
+        x += h;
+        console.log('puto el que lo lea!')
+        console.log("Este es h parseado: "+parseFloat(h))
+        console.log("Este es h: "+h)
         y = yn;
 
     }
 
 }
 
-function euler_mejorado(x, y, h) {
+function euler_mejorado(x,xmax, y, h) {
 
     var yp, yc, funtion;
 
@@ -61,7 +70,7 @@ function euler_mejorado(x, y, h) {
     var i = 0;
 
     // Este es Euler Mejorado con 1/2; Heuhn
-    while (x <= 2) {
+    while (x < xmax) {
         i++;
         fun = parser.eval('f(' + x + ',' + y + ')');
 
@@ -69,21 +78,22 @@ function euler_mejorado(x, y, h) {
         newfun = parser.eval('f(' + (x + h) + ',' + yp + ')');
         yc = y + (1 / 2) * (fun + newfun) * h;
         console.log("iteracción # " + i + " Yn+1 = " + yc + " Yn = " + y + " x = " + x + "\n");
-        $(".tbody").prepend(" <tr> <th>" + yc + "</th> <th>" + y + "</th> <th>" + x + "</th>  </tr>")
+        
+        $(".tbody").prepend(`<tr><th>${yc}</th><th>${y}</th><th>${x.toFixed(2)}</th></tr>`);
 
         x += h;
         y = yc;
     }
 }
 
-function ralston(x, y, h) {
+function ralston(x,xmax, y, h) {
     var yp1, yp2, yc;
 
     // Estas son las condiciones para el 2b
     var i = 0;
 
     // Este es Euler Mejorado; Ralston
-    while (x <= 2) {
+    while (x < xmax) {
         i++;
         //Este es el k1
         yp1 = parser.eval('f(' + x + ',' + y + ')');
@@ -93,7 +103,7 @@ function ralston(x, y, h) {
         //Aquì se evalua para hallar yi+1=yi+(k1+2*k2)*h/3
         yc = y + (1 / 3) * (yp1 + 2 * yp2) * h;
         console.log("iteracción # " + i + " Yn+1 = " + yc + " Yn = " + y + " x = " + x + "\n");
-        $(".tbody").prepend(" <tr> <th>" + yc + "</th> <th>" + y + "</th> <th>" + x + "</th>  </tr>")
+        $(".tbody").prepend(`<tr><th>${yc}</th><th>${y}</th><th>${x.toFixed(2)}</th></tr>`);
 
         x += h;
         y = yc;
